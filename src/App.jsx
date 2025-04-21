e945c1a94399cdb272eb82d89c4c798fe73c0328
Achla <achla2234@gmail.com>
2025-04-22 00:56:43 +0530

    updated history for making responses short

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,12 @@ const App = () => {
       setChatHistory(prev => [...prev.filter(msg => msg.text !== "thinking....."),{role:"model",text}])
     }
 
-    history = history.map(({role,text})=>({role,parts:[{text}]}))
+    // history = history.map(({role,text})=>({role,parts:[{text}]}))
+    history = [
+      { role: "system", parts: [{ text: "Respond briefly and concisely." }] },
+      ...history.map(({ role, text }) => ({ role, parts: [{ text }] }))
+    ]
+    
     
     const requestOptions={
       method : "POST",
@@ -91,99 +96,3 @@ const App = () => {
 }
 
 export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
